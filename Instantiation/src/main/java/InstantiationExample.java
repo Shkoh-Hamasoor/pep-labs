40c6021a512d4c0e5c19580c7e163ffd5fe13329
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2022-12-27 16:45:07 +0000

    Saved progress on Instantiation 2022-12-27 16:45:07 +0000

--- Instantiation/src/main/java/InstantiationExample.java
+++ Instantiation/src/main/java/InstantiationExample.java
@@ -18,6 +18,8 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        animal boxerDog = new animal();
+
+        return boxerDog;
     }
 }
