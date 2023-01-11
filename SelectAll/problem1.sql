91e4f96f87d81f6927a98379a83fd1d1ee135524
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2023-01-11 19:24:49 +0000

    Saved progress on SelectAll 2023-01-11 19:24:49 +0000

---
+++ SelectAll/problem1.sql
@@ -0,0 +1,15 @@
+/**
+     *  Assignment: write the SQL statement in the problem1.sql file to retrieve all the rows and columns from the
+     *  table "song". The db table we will utilize for this problem is the "song" table below
+     *
+     *  Song Table Diagram:
+     *  |      title        |        artist         |
+     *  ---------------------------------------------
+     *  |"Let it be"        |Beatles                |
+     *  |"Hotel California" |Eagles                 |
+     *  |"Kashmir"          |Led Zeppelin           |
+     *
+     */
+
+
+SELECT * FROM song;
