e72119c0df45299722aaef62110a83a492487835
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2023-01-30 18:18:20 +0000

    Saved progress on CombineArrays 2023-01-30 18:18:20 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -8,7 +8,11 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        int a = arr1.length;
+        int b = arr2.length;
+        int [] c = new int [a + b];
+        System.arraycopy(arr1, 0, c, 0, a);
+        System.arraycopy(arr2, 0, c, a, b);
+        return c;
     }
 }
